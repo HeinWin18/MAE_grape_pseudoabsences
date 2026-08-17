f4af0ff5c5c936dabd3046e6b30ea545ab2759cf
Hein Win <121010724+HeinWin18@users.noreply.github.com>
2026-08-17 16:02:26 -0700

    Implement compatibility fix for timm with PyTorch
    Added compatibility fix for timm==0.3.2 with modern PyTorch.

--- main_pretrain.py
+++ main_pretrain.py
@@ -19,14 +19,28 @@ import tifffile
 from torch.utils.data import Dataset
 
 import numpy as np
-import timm
 import torch
+
+# Compatibility fix for timm==0.3.2 with modern PyTorch
+import sys
+import types
+import collections.abc
+
+torch_six = types.ModuleType("torch._six")
+torch_six.container_abcs = collections.abc
+torch_six.string_classes = (str,)
+torch_six.int_classes = (int,)
+sys.modules["torch._six"] = torch_six
+
+import timm
+import torch.backends.cudnn as cudnn
+
 import torch.backends.cudnn as cudnn
 import torchvision.datasets as datasets
 import torchvision.transforms as transforms
 from torch.utils.tensorboard import SummaryWriter
 
-#assert timm.__version__ == "0.3.2"  # version check
+assert timm.__version__ == "0.3.2"  # version check
 import timm.optim.optim_factory as optim_factory
 
 import util.misc as misc
